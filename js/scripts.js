7474f23087dabe4bbf76749ffb8ab82ca8cbea3f
Chris Berry and Mike Gallegos <student@epicodus.com>
2016-01-13 10:43:34 -0800

    display branches and link

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-// $(function() {
-//   var animal = prompt("What is your favorite animal?");
+$(document).ready(function() {
+  var age = parseInt(prompt("How old are you?"));
+
+  if (age >= 18) {
+    $('#adultcontent').show();
+  } else {
+    $('#under-18').show();
+  }
+});
+
+
+// $(document).ready(function() {
+//   var age = parseInt(prompt("What is your age?"));
+//
+// });
+// //   var animal = prompt("What is your favorite animal?");
 //
 //   if (animal === "turtle") {
 //     $('#turtle').show();
