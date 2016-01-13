277555c9aad1aaa6c1482878dd84636bdc743fa8
Chris Berry and Mike Gallegos <student@epicodus.com>
2016-01-13 09:57:07 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// $(function() {
+//   var animal = prompt("What is your favorite animal?");
+//
+//   if (animal === "turtle") {
+//     $('#turtle').show();
+//   } else if (animal === "bird") {
+//     $('#bird').show();
+//   }
+//     else if (animal === "snake") {
+//     $('#snake').show();
+//   }
+// else {
+// alert("Sorry this is currently not an option, please try again");
+// var animal = prompt("What is your favorite animal?");
+// }
+// });
